5b77961d46dc38ac41d1ca3eaf7465e93a26d69c
liuchao <php_liuchao@163.com>
2017-11-22 16:09:00 +0800

    增加配置文件

---
+++ src/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Operator;
+
+class Config{
+    /**
+     * 魔蝎配置
+     *
+     * @var array
+     */
+    public static $moxie = [
+        'apiKey' => '',
+        'token' => '',
+    ];
+
+    /**
+     * 闪银配置
+     *
+     * @var array
+     */
+    public static $wecash = [
+        'source' => '',
+        'token' => '',
+    ];
+
+}
